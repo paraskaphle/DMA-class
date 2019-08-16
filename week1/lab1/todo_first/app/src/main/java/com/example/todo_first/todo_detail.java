459d02cdf0132e40c0d1ccfe99bc87dc38ea2344
paraskaphle <pallabkaphle@gmail.com>
2019-08-16 12:41:52 +0545

    first cmmit

---
+++ week1/lab1/todo_first/app/src/main/java/com/example/todo_first/todo_detail.java
@@ -0,0 +1,14 @@
+package com.example.todo_first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class todo_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_detail);
+    }
+}
