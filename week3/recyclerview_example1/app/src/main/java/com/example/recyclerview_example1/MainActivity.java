54cebc85a00b47edacc458cc03563526d7369ba8
paraskaphla <pallabkaphle@gmail.com>
2019-10-14 12:06:01 +0545

    message

---
+++ week3/recyclerview_example1/app/src/main/java/com/example/recyclerview_example1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.recyclerview_example1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.recyclerview_example1.WordListAdapter;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArrayList<String> wordlist;
+    private WordListAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wordlist = new ArrayList<>();
+        for (int i = 0; i<20; i++){
+            wordlist.add("word" + 1);
+        }
+        recyclerView = findViewById(R.id.word_list);
+        adapter = new WordListAdapter(wordlist);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
